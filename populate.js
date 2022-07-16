7e028102ef1e8cdbfc421f1ce71958f8db033f47
Holutunde <Holutunde99@gmail.com>
2022-07-16 22:32:53 +0100

    populate

--- populate.js
+++ populate.js
@@ -3,13 +3,13 @@ require('dotenv').config()
 const connectDB = require('./database/db')
 const Product = require('./server/models/productSchema')
 
-const jsonProducts = require('./products.json')
+const Products = require('./products')
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI)
     await Product.deleteMany()
-    await Product.create(jsonProducts)
+    await Product.create(Products)
     console.log('Success!!!!')
     process.exit(0)
   } catch (error) {
