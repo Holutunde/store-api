d39ba622acd3c430c81df8be54c59db01720dc9a
Holutunde <Holutunde99@gmail.com>
2022-07-18 15:04:48 +0100

    product query with sort and fields

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 require('dotenv').config()
+require('express-async-errors')
 const connectDB = require('./database/db')
 const products = require('./server/routes/products')
 const notFound = require('./middleware/notFound')
+const errorHandler = require('./middleware/errorHandler')
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('./public'))
@@ -13,6 +15,7 @@ app.use(express.json())
 app.use('/store', products)
 
 app.use(notFound)
+app.use(errorHandler)
 
 const port = process.env.PORT || 5000
 
--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,10 +1,40 @@
 const Product = require('../models/productSchema')
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({}).sort('price')
+  const products = await Product.find({}).sort('-name price')
   res.status(200).json({ products, nbHits: products.length })
 }
-const getAllQuery = (req, res) => {}
+const getAllQuery = async (req, res) => {
+  const { featured, company, name, sort, fields } = req.query
+  const queryObject = {}
+
+  if (featured) {
+    queryObject.featured = featured === 'true' ? true : false
+  }
+  if (company) {
+    queryObject.company = company
+  }
+  if (name) {
+    queryObject.name = { $regex: name, $options: 'i' }
+  }
+
+  let result = Product.find(queryObject)
+  // sort
+  if (sort) {
+    const sortList = sort.split(',').join(' ')
+    result = result.sort(sortList)
+  } else {
+    result = result.sort('createdAt')
+  }
+
+  if (fields) {
+    const fieldsList = fields.split(',').join(' ')
+    result = result.select(fieldsList)
+  }
+
+  const products = await result
+  res.status(200).json({ products, nbHits: products.length })
+}
 
 module.exports = {
   getAllProducts,
