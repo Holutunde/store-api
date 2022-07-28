f971240857cd7a4148915a2334ef0b09973508b3
Holutunde <Holutunde99@gmail.com>
2022-07-28 19:12:06 +0100

    modify controllers

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,9 +1,9 @@
 const Product = require('../models/productSchema')
 
 const getAllProducts = async (req, res) => {
-  const products = await Product.find({price: {$gt:10}})
+  const products = await Product.find({ price: { $gt: 10 } })
     .sort('-name price')
-   // .select('name price')
+    //.select('name price')
     .limit(10)
     .skip(3)
 
@@ -23,7 +23,6 @@ const getAllQuery = async (req, res) => {
     queryObject.name = { $regex: name, $options: 'i' }
   }
 
-
   //filters
   if (numericFilters) {
     const operatorMap = {
@@ -32,25 +31,25 @@ const getAllQuery = async (req, res) => {
       '=': '$eq',
       '<': '$lt',
       '<=': '$lte',
-    };
-    const regEx = /\b(<|>|>=|=|<=)\b/g;
+    }
+    const regEx = /\b(<|>|>=|=|<=)\b/g
     let filters = numericFilters.replace(
       regEx,
-      (match) => `-${operatorMap[match]}-`
-    );
-    const options = ['price', 'rating'];
+      (match) => `-${operatorMap[match]}-`,
+    )
+    const options = ['price', 'rating']
     filters = filters.split(',').forEach((item) => {
       const [field, operator, value] = item.split('-')
       if (options.includes(field)) {
         queryObject[field] = { [operator]: Number(value) }
       }
-      console.log(filters)
-    });
+      //console.log(filters)
+    })
   }
 
-  console.log(queryObject);
+  console.log(queryObject)
   let result = Product.find(queryObject)
-  
+
   // sort
   if (sort) {
     const sortList = sort.split(',').join(' ')
@@ -58,7 +57,7 @@ const getAllQuery = async (req, res) => {
   } else {
     result = result.sort('createdAt')
   }
- 
+
   // fields
   if (fields) {
     const fieldsList = fields.split(',').join(' ')
